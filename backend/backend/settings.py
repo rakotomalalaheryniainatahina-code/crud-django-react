af3c1a6a2b03d5d05f6d95a236a8b8eab488ad14
rakotomalalaheryniainatahina-code <rakotomalalaheryniainatahina@gmail.com>
2025-09-28 09:40:55 +0300

    push 28

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0lh8gol4qwt*n(^5f_z3nkxl&glssoutn6ytg*qbz96@x)wrhq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
